24d2466f58416ea7f4822fa9ba0266381dcda85b
David Wong <neoplastic@gmail.com>
2016-08-03 09:42:24 +1000

    Updated for Xcode 8.0 beta-4 Swift 3.0 compatability.

--- Source/RxOptionalError.swift
+++ Source/RxOptionalError.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-public enum RxOptionalError: ErrorProtocol, CustomStringConvertible {
-    case FoundNilWhileUnwrappingOptional(Any.Type)
-    case EmptyOccupiable(Any.Type)
+public enum RxOptionalError: Swift.Error, CustomStringConvertible {
+    case foundNilWhileUnwrappingOptional(Any.Type)
+    case emptyOccupiable(Any.Type)
 
     public var description: String {
         switch self {
-        case .FoundNilWhileUnwrappingOptional(let type):
+        case .foundNilWhileUnwrappingOptional(let type):
            return "Found nil while trying to unwrap type <\(String(type))>"
-        case .EmptyOccupiable(let type):
+        case .emptyOccupiable(let type):
             return "Empty occupiable of type <\(String(type))>"
         }
     }
